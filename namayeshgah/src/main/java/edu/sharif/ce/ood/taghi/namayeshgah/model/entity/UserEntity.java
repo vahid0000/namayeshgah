ab2ff3bfce2c6849519f010ebd4b98ee475e2bcd
Amir Abbas Sadeghian <amir.abbas00@gmail.com>
2013-07-11 12:04:42 +0430

    first sync

--- namayeshgah/src/main/java/edu/sharif/ce/ood/taghi/namayeshgah/model/entity/UserEntity.java
+++ namayeshgah/src/main/java/edu/sharif/ce/ood/taghi/namayeshgah/model/entity/UserEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 @Entity(name="user")
 public class UserEntity extends BaseEntity<Integer> {
 
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
